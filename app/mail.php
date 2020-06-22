3ae9b9bf0565fdc4c0c507e318bae8727ba2c3b7
viktoriazaharova <zaharova181114@gmail.com>
2020-06-22 12:01:25 +0300

    edit

--- app/mail.php
+++ app/mail.php
@@ -5,8 +5,9 @@ $sitename = "АГЕНТ РОСТА";
 
 $phone = trim($_POST["phone"]);
 $email = trim($_POST["mail"]);
+$message = trim($_POST["message"]);
 
-$message = "Телефон: $phone \nE-mail: $email";
+$message = "Телефон: $phone \nE-mail: $email \nВаши пожелания: $message";
 
 $pagetitle = "Заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
